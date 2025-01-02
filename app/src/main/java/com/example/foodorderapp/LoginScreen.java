19e4da77a55886a286b6c4ca1f01532178ca3c33
Ragasri05 <you@example.com>
2025-01-02 21:18:36 +0530

    login and register activities

--- app/src/main/java/com/example/foodorderapp/LoginScreen.java
+++ app/src/main/java/com/example/foodorderapp/LoginScreen.java
@@ -1,24 +1,76 @@
 package com.example.foodorderapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class LoginScreen extends AppCompatActivity {
+    EditText LEmail, LPassword;
+    TextView LTextView;
+    Button LButton;
+    FirebaseAuth fb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        LEmail = findViewById(R.id.LEmail);
+        LPassword = findViewById(R.id.LPassword);
+        LButton = findViewById(R.id.LButton);
+        LTextView = findViewById(R.id.LTextView);
+        fb = FirebaseAuth.getInstance();
+
+        LButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = LEmail.getText().toString().trim();
+                String password = LPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    LEmail.setError("Please enter your email address");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    LPassword.setError("Please Enter your password");
+                    return;
+                }
+                fb.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(LoginScreen.this, "error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
+        LTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RegisterScreen.class));
+                finish();
+            }
         });
     }
 }
---
+++ app/src/main/java/com/example/foodorderapp/RegisterScreen.java
@@ -0,0 +1,90 @@
+package com.example.foodorderapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterScreen extends AppCompatActivity {
+
+    FirebaseAuth fb;
+    EditText REmail, RPassword;
+    TextView RTextView;
+    Button RButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register_screen);
+
+            fb = FirebaseAuth.getInstance();
+            REmail = findViewById(R.id.Ret1);
+            RPassword = findViewById(R.id.Ret2);
+            RTextView = findViewById(R.id.Rtv1);
+            RButton = findViewById(R.id.Rb1);
+
+            // checking if the user already logged in
+            if (fb.getCurrentUser() != null){
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+
+            RButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String email = REmail.getText().toString().trim();
+                    String password = RPassword.getText().toString().trim();
+
+                    if (TextUtils.isEmpty(email)) {
+                        REmail.setError("Please enter your email address");
+                        return;
+                    }
+                    if (TextUtils.isEmpty(password)) {
+                        RPassword.setError("Please Enter your password");
+                        return;
+                    }
+
+                    // creating a new user.
+                    //registering the user with firebase;
+                    fb.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(RegisterScreen.this, "User Created", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            }
+                            else{
+                                Toast.makeText(RegisterScreen.this, "Opps!, an error occured."+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                }
+            });
+
+            RTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(getApplicationContext(), LoginScreen.class));
+                    finish();
+                }
+            });
+    }
+}
